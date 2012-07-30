cab6156e0ee476b665c9e6c06006e68f3342324a
sean <sean@antfarm.de>
2012-07-30 23:42:53 +0200

    added install script and changed readme

---
+++ bin/install_libpd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR=`dirname $0`
+PROJECT_ROOT=`cd ${SCRIPT_DIR}/.. && pwd`
+
+cd ${PROJECT_ROOT}/vendor
+git clone https://github.com/libpd/pd-for-ios.git
+
+cd pd-for-ios
+git submodule init
+git submodule update
+
+cd libpd
+git apply ${PROJECT_ROOT}/vendor/libpd_patch.diff.txt
