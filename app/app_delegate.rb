917f8b7bbbd72a956cfadd0eab7165fd47e05976
sean <sean@antfarm.de>
2012-07-31 00:52:06 +0200

    added basic pd patch

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,20 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    UIAlertView.new.tap do |alert|
+      alert.message = "antfarm/rubymotion-libpd"
+    end.show
+
+    @controller = PdAudioController.alloc.init
+    @controller.configurePlaybackWithSampleRate(44100, 
+      numberChannels: 2, inputEnabled:false, mixingEnabled: true) 
+    @controller.configureTicksPerBuffer(64)
+    
+    @dispatcher = PdDispatcher.alloc.init
+    PdBase.setDelegate(@dispatcher) 
+    @patch = PdBase.openFile('test.pd', path:NSBundle.mainBundle.resourcePath)
+    
+    @controller.active = true
+
     true
   end
 end
